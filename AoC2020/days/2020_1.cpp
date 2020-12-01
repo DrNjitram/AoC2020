25b041654a12e9963f3ec94a3b1b71acce29b720
dr_njitram <martijn.gobes@gmail.com>
2020-12-01 12:46:05 +0100

    day 1 crappy set implementation

--- AoC2020/days/2020_1.cpp
+++ AoC2020/days/2020_1.cpp
@@ -6,42 +6,23 @@
 #include <iomanip>
 #include <fstream>
 #include <chrono> 
+#include <set>
 
 using namespace std;
 
-int main() {
-
-	ifstream input("C:\\Users\\Martijn\\Desktop\\AoC2020\\AoC2020\\input\\2020_1.txt");
-
-	stringstream buffer;
-	buffer << input.rdbuf();
-	istringstream is(buffer.str());
-
-	vector<int> numbers;
-
-	int n;
-
-	while (is >> n) {
-		numbers.push_back(n);
-	}
-
-	auto start = chrono::high_resolution_clock::now();
-
-	int answer1_1;
-	int answer1_2;
-
-	int answer2_1;
-	int answer2_2;
-	int answer2_3;
-
+void solutionvect(vector<int> numbers) {
+	int answer1;
+	int answer2;
 	int min = 9999;
+	int target1;
+	int target2;
+	vector<int> second_numbers;
+
 	for (int& val : numbers) {
 		if (val < min) min = val;
 	}
 
 	min = 2020 - min;
-
-	vector<int> second_numbers;
 
 	for (int& val : numbers) {
 		if (val <= min) {
@@ -50,9 +31,6 @@ int main() {
 	}
 
 	numbers = second_numbers;
-	
-	int target1;
-	int target2;
 
 	for (int& val1 : numbers) {
 		if (val1 > min) continue;
@@ -60,8 +38,7 @@ int main() {
 		for (int& val2 : numbers) {
 
 			if (val2 == target1) {
-				answer1_1 = val1;
-				answer1_2 = val2;
+				answer1 = val1 * val2;
 				continue;
 			}
 			else if (val1 + val2 > min) {
@@ -71,23 +48,109 @@ int main() {
 			target2 = 2020 - val1 - val2;
 			for (int& val3 : numbers) {
 				if (val3 == target2) {
-					answer2_1 = val1;
-					answer2_2 = val2;
-					answer2_3 = val3;
+					answer2 = val1 * val2 * val3;
 				}
 			}
 
 		}
 	}
+
+	//cout << answer1 << endl;
+	//cout << (answer1 == 437931) << endl;
+	//cout << answer2 << endl;
+	//cout << (answer2 == 157667328) << endl;
+}
+
+void solutionset(set<int> numbers) {
+	int answer1;
+	int answer2;
+	int target1;
+	int target2;
+	set<int> second_numbers;
+
+	int min = 2020 - *numbers.begin();
+
+	for (const int& val : numbers) {
+		if (val <= min) {
+			second_numbers.insert(val);
+		}
+	}
+
+	numbers = second_numbers;
+
+	for (const int& val : numbers) {
+		if (val > min) continue;
+		target1 = 2020 - val;
+		for (const int& val2 : numbers) {
+
+
+			if (val2 == target1) {
+				answer1 = val * val2;
+				continue;
+			}
+			else if (val + val2 > min) {
+				continue;
+			}
+
+			target2 = 2020 - val - val2;
+
+			if (numbers.find(target2) != numbers.end())
+				answer2 = val * val2 * target2;
+		}
+	}
+
+	//cout << answer1 << endl;
+	//cout << (answer1 == 437931) << endl;
+	//cout << answer2 << endl;
+	//cout << (answer2 == 157667328) << endl;
+}
+
+
+int main() {
+
+	ifstream input("C:\\Users\\Martijn\\Desktop\\AoC2020\\AoC2020\\input\\2020_1.txt");
+
+	stringstream buffer;
+	buffer << input.rdbuf();
+	istringstream is(buffer.str());
+
+	set<int> numberss;
+	vector<int> numbersv;
+
+	int n;
+	int iters = 100;
+
+	while (is >> n) {
+		numbersv.push_back(n);
+		numberss.insert(n);
+	}
+
+	auto start = chrono::high_resolution_clock::now();
+
+	for (int i = 0; i < iters; i++) {
+		solutionvect(numbersv);
+	}
+
 	auto stop = chrono::high_resolution_clock::now();
 
 	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 
-	cout << answer1_1 * answer1_2 << endl;
-	cout << (answer1_1 * answer1_2 == 437931) << endl;
-	cout << answer2_1 * answer2_2 * answer2_3 << endl;
-	cout << (answer2_1 * answer2_2 * answer2_3 == 157667328 )<< endl;
+	cout << "Vect: " << duration.count()/ iters << endl;
 
-	cout << duration.count() << endl;
+
+	start = chrono::high_resolution_clock::now();
+	
+	for (int i = 0; i < iters; i++) {
+		solutionset(numberss);
+	}
+
+	stop = chrono::high_resolution_clock::now();
+
+	duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+
+
+
+	cout << "Set: " << duration.count() / iters << endl;
 
 }
+
