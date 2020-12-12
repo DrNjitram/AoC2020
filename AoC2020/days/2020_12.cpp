fdbc1208ff7bf303eb67b258ee9f0339850b261b
dr_njitram <martijn.gobes@gmail.com>
2020-12-12 12:52:02 +0100

    Day 12 Part 2

--- AoC2020/days/2020_12.cpp
+++ AoC2020/days/2020_12.cpp
@@ -8,8 +8,29 @@
 #include <iterator>
 #include <map>
 #include <set>
+#include <math.h>
 
 using namespace std;
+
+int ship_pos[] = { 0, 0 }; //x, y
+int way_pos[] = { 10, 1 };
+int direction = 0; //0 E, 1 S, 2 W, 3, N
+
+
+void rotate(int degrees) {
+	int result[2];
+
+	double angle = degrees * ((atan(1) * 4) / 180);
+
+	int c = cos(angle);
+	int s = sin(angle);
+
+	result[0] = (way_pos[0] * c) - (way_pos[1] * s);
+	result[1] = (way_pos[1] * c) + (way_pos[0] * s);
+
+	way_pos[0] = result[0];
+	way_pos[1] = result[1];
+}
 
 int main() {
 	ifstream input("C:\\Users\\Martijn\\Desktop\\AoC2020\\AoC2020\\input\\2020_12.txt");
@@ -26,36 +47,22 @@ int main() {
 		instructions.push_back(pair<char, int>(x[0], stoi(x.substr(1, x.length()))));
 	
 
-	int pos[] = { 0, 0 }; //x, y
-	int direction = 0; //0 E, 1 S, 2 W, 3, N
-
 	for (pair<char, int> p : instructions) {
 
 		if (p.first == 'F') {
-			switch (direction) {
-				case 0:
-					pos[0] += p.second;
-					break;
-				case 1:
-					pos[1] -= p.second;
-					break;
-				case 2:
-					pos[0] -= p.second;
-					break;
-				case 3:
-					pos[1] += p.second;
-					break;
-			}
+			ship_pos[0] += p.second * way_pos[0];
+			ship_pos[1] += p.second * way_pos[1];
 		}
-		else if (p.first == 'N') pos[1] += p.second;
-		else if (p.first == 'E') pos[0] += p.second;
-		else if (p.first == 'S') pos[1] -= p.second;
-		else if (p.first == 'W') pos[0] -= p.second;
-		else if (p.first == 'L') direction = (((size_t) direction - (p.second/90) ) % 4);
-		else if (p.first == 'R') direction = (((size_t)direction + (p.second / 90)) % 4);;
-				
+		else if (p.first == 'N') way_pos[1] += p.second;
+		else if (p.first == 'E') way_pos[0] += p.second;
+		else if (p.first == 'S') way_pos[1] -= p.second;
+		else if (p.first == 'W') way_pos[0] -= p.second;
+		else if (p.first == 'L') rotate(p.second);
+		else if (p.first == 'R') rotate(-1 * p.second);
+		
+
 	}
 
-	cout << abs(pos[0]) + abs(pos[1]) << endl;
+	cout << abs(ship_pos[0]) + abs(ship_pos[1]) << endl;
 }
 
