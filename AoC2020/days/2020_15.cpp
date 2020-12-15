8909fa5267c1cda21f9d0f9335756dbce30eeb98
dr_njitram <martijn.gobes@gmail.com>
2020-12-15 12:15:21 +0100

    Unordered map goes brrrrrrrrrrrrrrrr

--- AoC2020/days/2020_15.cpp
+++ AoC2020/days/2020_15.cpp
@@ -10,10 +10,11 @@
 #include <set>
 #include <bitset>
 #include <numeric>
+#include <unordered_map>
 
 using namespace std;
 
-map<int, pair<int, int>> turns; // value seen last at turns n0 and before that at turn n1
+unordered_map<int, pair<int, int>> turns; // value seen last at turns n0 and before that at turn n1
 
 int main() {
 	istringstream is("6 4 12 1 20 0 16");
@@ -24,21 +25,21 @@ int main() {
 	while (is >> last_said)
 		turns[last_said] = pair<int, int>(++i, 0);
 
-	int turn_limit = 30000001;
 
 	auto start = chrono::high_resolution_clock::now();
+
+	int turn_limit = 30000001;
 
 	for (int i = turns.size() + 1; i < turn_limit; i++) {
 		pair<int, int> p = turns[last_said];
 		last_said = p.second == 0 ? 0 : p.first - p.second;
-		//if (i % 100000 == 0) cout << i << endl;
 		turns[last_said] = pair<int, int>(i, turns[last_said].first);
 	}
 
 	auto stop = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::seconds>(stop - start);
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 
 	cout << last_said << endl;
 
-	cout << "Time(ss): " << duration.count() << endl;
+	cout << "Time(s): " << duration.count() << endl;
 }
