fd310e9687150cfdb068bd52623bfe6fab1c7633
dr_njitram <martijn.gobes@gmail.com>
2020-12-15 11:03:00 +0100

    Day 15 Part 2

--- AoC2020/days/2015_15.cpp
+++ AoC2020/days/2015_15.cpp
@@ -13,34 +13,23 @@
 
 using namespace std;
 
-vector<int> turns;
-
-int find_val(int v, int turn) {
-	for (int i = turns.size() - 2; i >= 0; i--) {
-		if (turns[i] == v) return turn - i - 1;
-	}
-	return 0;
-}
+map<int, pair<int, int>> turns; // value seen last at turns n0 and before that at turn n1
 
 int main() {
+	istringstream is("6 4 12 1 20 0 16");
 
-	string input = "0 3 6";
-	istringstream is(input);
+	int i = 0;
+	int last_said;
 
-	int n;
-	while (is >> n) {
-		turns.push_back(n);
-		//cout << n << endl;
-	}
+	while (is >> last_said) 
+		turns[last_said] = pair<int, int>(++i, 0);	
 
-	int turn_limit = 30000000;
-	int last_said = turns[turns.size() - 1];
-
-
-	for (int i = turns.size(); i < turn_limit; i++) {
-		last_said = find_val(last_said, i);
-		//cout <<  i + 1 << ": " << last_said << endl;
-		turns.push_back(last_said);
+	int turn_limit = 2021;
+	
+	for (int i = turns.size() + 1; i < turn_limit; i++) {
+		last_said = turns[last_said].second == 0 ? 0 : turns[last_said].first - turns[last_said].second;
+		if (i % 100000 == 0) cout << i << endl;
+		turns[last_said] = pair<int, int>(i, turns[last_said].first);
 	}
 
 	cout << last_said << endl;
